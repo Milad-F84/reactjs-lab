04cfd72356743c1d77adc5492268dfb911d41c77
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-02-15 18:59:22 +0330

    cards data added

--- 002-json/src/App.jsx
+++ 002-json/src/App.jsx
@@ -1,5 +1,29 @@
-
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 export default function App() {
+  const [cards, setCards] = useState([]);
 
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setCards(json));
+  }, []);
+
+  return (
+    <div>
+      {cards.map((card) => {
+        const [id, title, price, category, description, image] = card;
+        return (
+          <Card
+            key={id}
+            title={title}
+            price={price}
+            category={category}
+            desc={description}
+            img={image}
+          />
+        );
+      })}
+    </div>
+  );
 }
-
