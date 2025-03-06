906f8f87f49bd8f8a9fca31915fd3c1d132bbcab
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-03-06 18:14:06 +0330

    home page and products complete

--- 004-flightio/src/components/SingleProduct.jsx
+++ 004-flightio/src/components/SingleProduct.jsx
@@ -1,3 +1,21 @@
-export default function SingleProduct(){
-    
+import { Box, Button } from "@mui/material";
+
+
+export default function SingleProduct({image, title ,price}){
+
+
+    return(
+        <>
+        <Box component="div" width="20rem" margin="5rem auto" boxShadow="0px 0px 10px 10px rgba(0,0,0,0.2)" padding="1.5rem" borderRadius="1rem">
+            <img src={image} alt="image" width="full"/>
+            <Box component="div" marginTop="3rem">
+                <h2 className="mb-[1rem] font-bold">{title}</h2>
+                <span>${price}</span>
+
+            </Box>
+            
+        </Box>
+        
+      </>
+    )
 }
--- 004-flightio/src/pages/HomePage.jsx
+++ 004-flightio/src/pages/HomePage.jsx
@@ -1,3 +1,41 @@
-export default function HomePage(){
-    
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import SearchBar from "../components/SearchBar";
+import SingleProductPage from "./SingleProductPage";
+import SingleProduct from "../components/SingleProduct";
+
+export default function HomePage() {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+
+    return () => {
+      console.log("home page unmounted");
+    };
+  }, []);
+
+  function handleProductClick(){
+    navigate("");
+  }
+
+  return(
+    <>
+    <SearchBar />
+    {
+        products.map((item) => (
+            <SingleProduct 
+            onclick={handleProductClick}
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            image={item.image}
+            />
+        ))
+    }
+    </>
+  )
 }
